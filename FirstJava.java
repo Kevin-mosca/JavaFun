95880e7b07d74a2a866dd669d7e525cf8c5a3e1e
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-06-07 11:40:05 -0700

    first commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava{
+    public static void main(String[] args){
+        System.out.println("My name is Kevin Mosca");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Gilroy CA");
+    }
+}
